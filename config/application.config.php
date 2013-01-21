7fc6cbf8b8e7a1558ad3f96d28fd145a1771a72a
Murga Nikolay <work@murga.kiev.ua>
2013-01-21 12:29:11 +0200

    Enable modules

--- config/application.config.php
+++ config/application.config.php
@@ -1,13 +1,15 @@
 <?php
 return array(
     'modules' => array(
-        'Application',
-        //'DoctrineModule',
+        'DoctrineModule',
+        'DoctrineORMModule',
+        'DoctrineMigrationsModule',
         'SmartyModule',
+        'Application',
     ),
     'module_listener_options' => array(
         'config_glob_paths' => array(
-            'config/autoload/{,*.}{global,local}.php',
+             'config/autoload/{,*.}{global,local}.php',
         ),
         'module_paths' => array(
             './module',
