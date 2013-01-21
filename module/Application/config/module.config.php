cbc606b64ac8b4434e70074b785cc78dc1973f68
Murga Nikolay <work@murga.kiev.ua>
2013-01-21 12:29:22 +0200

    Add Requariments

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -89,4 +89,19 @@ return array(
             __DIR__ . '/../view',
         ),
     ),
+// Doctrine config
+    'doctrine' => array(
+        'driver' => array(
+            'application_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/Application/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Application\Entity' => 'application_driver'
+                )
+            )
+        ),
+    ),
 );
